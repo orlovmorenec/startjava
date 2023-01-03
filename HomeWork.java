e6b5103991e175862bad151928e14556a0f39a71
Arthur <orlovmorenec@gmail.com>
2023-01-03 21:41:47 +0300

    add next homework from internet

--- HomeWork.java
+++ HomeWork.java
@@ -16,5 +16,14 @@ public class HomeWork {
         } else {
             System.out.print("Число равно 1");
         }
+/*
+Необходимо, чтоб программа выводила на экран вот такую последовательность:
+7 14 21 28 35 42 49 56 63 70 77 84 91 98
+ */
+        System.out.println("\nЗадача № 2");
+        number = 7;
+        for (int i = 1; i < 15; i++) {
+            System.out.print(number * i + " ");
+        }
     }
 }
